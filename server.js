66f2a771cc0a37584d8836ad7dc8f68bec26cfed
Ronak-Ronu <ronakronu02@gmail.com>
2024-09-09 01:11:22 +0530

    initial commit

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ async function main() {
     console.log('db connected');
 }
 
-app.use(cors());
+
+const corsOptions = {
+  origin: '*', 
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 
 app.use('/api', quizResultRouter);
